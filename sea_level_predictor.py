cf77fa87334f5290582de4e14eea007fcf792211
a.zare <arezoo.zare9604@gmail.com>
2022-09-05 19:20:09 +0430

    update matplot

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -10,23 +10,37 @@ def draw_plot():
     df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
-    df = df.append({"Year": 2050, "CSIRO Adjusted Sea Level":0}, ignore_index=True)
-    x = df["Year"]
-    y = df["CSIRO Adjusted Sea Level"]
-    plt.scatter(x, y)
+    original_x = df["Year"]
+    original_y = df["CSIRO Adjusted Sea Level"]
+    plt.scatter(original_x, original_y)
 
     # Create first line of best fit
+    mask = df["Year"] > 2000
+    df = df[mask]
+    x = df["Year"]
+    y = df["CSIRO Adjusted Sea Level"]
     slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
 
-    extended_years = np.arange(2000, 2050, 10)
+    extended_years = np.arange(2000, 2060, 10)
     predicts = [intercept + slope * new_x for new_x in extended_years]
+    #
+    # x = np.concatenate((x, extended_years))
+    # y = np.concatenate((y, predicts))
 
-    plt.plot(extended_years, predicts, 'r', label='fitted line')
+    # slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
+
+    # plt.plot(extended_years, predicts, 'r', label='fitted line')
+
+    plt.plot(extended_years, predicts, 'r')
+    # plt.legend()
+    fmt = lambda x: "{:.1f}".format(x)
+
+    plt.xticks(np.arange(1850, 2100, 25), [fmt(i) for i in np.arange(1850, 2100, 25)])
+    plt.show()
+    plt.title("Rise in Sea Level")
     plt.xlabel("Year")
     plt.ylabel("Sea Level (inches)")
-    plt.title("Rise in Sea Level")
-    plt.legend()
-    plt.show()
+
     # Create second line of best fit
 
     # Add labels and title
